f125a3eadec4d3737789904512c76a4f79088381
YourName <youremail@gmail.com>
2026-03-08 16:06:29 +0530

    UC3: Print OOPS to console

--- OOPSBannerApp/src/OOPSBannerApp.java
+++ OOPSBannerApp/src/OOPSBannerApp.java
@@ -1,11 +1,10 @@
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("   **  "  +  "      **  "  + "  ******" + "    ******");
-        System.out.println("**    **" + "  **    **" + " **   **"+ "  **");
-        System.out.println("**    **" + "  **    **" + " **   **"+ "   **");
-        System.out.println("**    **" + "  **    **" + " ******" + "      ***");
-        System.out.println("**    **" + "  **    **" + " **"     + "            **");
-        System.out.println("**    **" + "  **    **" + " **    " + "       **");
-        System.out.println("   **  "  +  "      **  "  + "  ** "    + "     ******");
+
+        // Printing OOPS to console
+        System.out.println("OOPS");
+
     }
 }
